8010665be7aaa5d3955eb8b4d4910be2432c7a94
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2024-09-23 23:28:36 +1030

    LO1-displaying values in the arrays

--- src/jaramillo_it20/Jaramillo_it20.java
+++ src/jaramillo_it20/Jaramillo_it20.java
@@ -7,6 +7,13 @@ public class Jaramillo_it20 {
     
     public static void main(String[] args) {
         String [] name = {"Almera", "Jane", "Pia"};
+    
+        
+         //Display values
+      System.out.print("First Name:" + name[0]);
+      System.out.print("Second Name:" + name[1]);
+      System.out.print("Third Name:" + name[2]);
     }
+    
     
 }
