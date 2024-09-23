49eaab89cd3a94dc629e2bbab6b0ddcb6ee331f0
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2024-09-23 23:27:21 +1030

    LO1-declaring string array

---
+++ src/jaramillo_it20/Jaramillo_it20.java
@@ -0,0 +1,12 @@
+
+package jaramillo_it20;
+
+
+public class Jaramillo_it20 {
+
+    
+    public static void main(String[] args) {
+        String [] name = {"Almera", "Jane", "Pia"};
+    }
+    
+}
