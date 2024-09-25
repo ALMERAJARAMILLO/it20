4ca845b21de419ea4d19c054fcc48624603e7f35
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2024-09-26 03:54:43 +1030

    Initializing variables

---
+++ src/ArrayList/ItemManager.java
@@ -0,0 +1,13 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ItemManager {
+
+    private ArrayList<String> items;
+    
+    //Constructor to initialize the Arraylist
+    public ItemManager() {
+        items = new ArrayList<>();
+    }
+}
---
+++ src/ArrayList/main.java
@@ -0,0 +1,15 @@
+package ArrayList;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Scanner a = new Scanner(System.in);
+        ItemManager list = new ItemManager();
+        //Initializing variables
+        String item1, item2, item3, newitem, finditem;
+        int index;
+    }
+}
