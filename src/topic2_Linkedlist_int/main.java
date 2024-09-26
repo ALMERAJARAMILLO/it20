ab339c5a5dfebb950b27cc8574808228cadc4caf
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2024-09-27 03:07:50 +1030

    Added method to delete and swap nodes in the list

--- src/topic2_Linkedlist_int/main.java
+++ src/topic2_Linkedlist_int/main.java
@@ -33,4 +33,75 @@ public class main {
         }
 
     }
+
+    public static void move() {
+        System.out.println("Enter the first index you want to swap: ");
+        int element1 = sc.nextInt();
+        System.out.println("Enter the second index you want to swap: ");
+        int element2 = sc.nextInt();
+
+        list.moveNodePointer(element1, element2);
+        list.printList();
+    }
+
+    public static void display() {
+        list.printList();
+    }
+
+    public static void delete() {
+        System.out.println("What Integer would you like to delete from this list?");
+        list.printList();
+        int element = sc.nextInt();
+        list.deleteByValue(element);
+        System.out.println("Integer '" + element + "' successfully deleted!");
+        System.out.print("List: ");
+        list.printList();
+    }
+
+    public static void main(String[] args) {
+
+        //Displaying Menu Choices
+        System.out.println("Welcome to Simple LinkedList Program!");
+        boolean exit = false;
+        while (!exit) {
+            System.out.println();
+            System.out.println("-----------------------");
+            System.out.println("1. Add Integer");
+            System.out.println("2. Move or Swap Integer");
+            System.out.println("3. Display");
+            System.out.println("4. Delete");
+            System.out.println("5. Exit");
+            System.out.println("-----------------------");
+            System.out.println();
+
+            try {
+                System.out.print("Response: ");
+                int choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        adding();
+                        break;
+                    case 2:
+                        move();
+                        break;
+                    case 3:
+                        display();
+                        break;
+                    case 4:
+                        delete();
+                        break;
+                    case 5:
+                        exit = true;
+                        System.out.println("Thank you for using Simple Linkedlist Program. Goodbye!");
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please select a valid option.");
+                }
+            } catch (Exception e) {
+                System.out.println("Error 404. Input Invalid");
+                break;
+            }
+
+        }
+    }
 }
