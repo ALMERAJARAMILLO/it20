baff350945578f17768e3b5363a2303119bbadb8
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2024-10-11 20:18:16 +0800

    Added data fields and constructor

---
+++ src/topic_3_stack/Stack.java
@@ -0,0 +1,14 @@
+package topic_3_stack;
+
+public class Stack {
+
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1;
+    }
+}
