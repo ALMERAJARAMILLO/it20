c310a7bc235132df8d9cbaa0f020d00d9ddf688d
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2024-10-16 20:07:05 +1100

    Added extends JFrame

--- src/topic_3_stack_calculator/StackCalculator.java
+++ src/topic_3_stack_calculator/StackCalculator.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package topic_3_stack_calculator;
 
-/**
- *
- * @author HOME
- */
-public class StackCalculator {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Stack;
+
+public class StackCalculator extends JFrame implements ActionListener {
+
+    private Stack<Integer> stack;
+    private JTextArea display;
+
+    public StackCalculator() {
+        stack = new Stack<>();
+        setupFrame();
+        setupDisplay();
+        setupButtons();
+    }
+
+    private void setupFrame() {
+        setTitle("Stack Calculator");
+        setSize(300, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout(10, 10));
+        setResizable(false);
+    }
+
+    private void setupDisplay() {
+        display = new JTextArea(3, 20);
+        display.setEditable(false);
+        display.setFont(new Font("Arial", Font.PLAIN, 16));
+        display.setMargin(new Insets(10, 10, 10, 10));
+        JScrollPane scrollPane = new JScrollPane(display);
+        add(scrollPane, BorderLayout.NORTH);
+    }
     
 }
